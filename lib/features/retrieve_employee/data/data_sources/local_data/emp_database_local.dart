ebc33b60a8ce138ce3eac9ed7e4ea85aa17e1c46
Madhukar Verma <madhukarjeeverma@yahoo.com>
2025-02-07 13:49:29 +0530

    usecase added for get emp list

--- lib/features/retrieve_employee/data/data_sources/local_data/emp_database_local.dart
+++ lib/features/retrieve_employee/data/data_sources/local_data/emp_database_local.dart
@@ -1,7 +1,7 @@
 import 'package:madhukar_app/features/retrieve_employee/data/models/emp_data_model.dart';
 
 class EmpDatabaseLocal {
-  Future<List<EmpDataModel>> getAllemployeeLocal() {
+  Future<List<EmpDataModel>> getAllEmployeeLocal() {
     return Future(() => []);
   }
 }
