6fc486f22a022647c727e3e5eefb3b70fa21bb6b
Madhukar Verma <madhukarjeeverma@yahoo.com>
2025-02-08 23:11:01 +0530

    calendar added

--- lib/features/retrieve_employee/presentation/pages/add_emp_detail_page.dart
+++ lib/features/retrieve_employee/presentation/pages/add_emp_detail_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:madhukar_app/config/util/emp_role.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../widgets/app_button.dart';
 
@@ -12,6 +14,22 @@ class AddEmployeeDetailPage extends StatefulWidget {
 
 class _AddEmployeeDetailPageState extends State<AddEmployeeDetailPage> {
   final TextEditingController _nameController = TextEditingController();
+
+  final CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _selectedDate = DateTime.now();
+  final DateFormat formatter = DateFormat('d MMM yyyy');
+
+  // void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
+  //   setState(() {
+  //     _selectedDate = args.value;
+  //   });
+  // }
+
+  // void _selectPresetDate(DateTime date) {
+  //   setState(() {
+  //     _selectedDate = date;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -98,23 +116,28 @@ class _AddEmployeeDetailPageState extends State<AddEmployeeDetailPage> {
               Row(
                 children: [
                   Expanded(
-                    child: Container(
-                      height: 40,
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: const Color(0xffE5E5E5)),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(2)),
-                      ),
-                      child: Row(children: [
-                        Image.asset(
-                          'assets/images/calendar.png',
-                          height: 24,
-                          width: 24,
+                    child: GestureDetector(
+                      onTap: () {
+                        selectDate();
+                      },
+                      child: Container(
+                        height: 40,
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: const Color(0xffE5E5E5)),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(2)),
                         ),
-                        const SizedBox(width: 13.8),
-                        const Text('Today'),
-                      ]),
+                        child: Row(children: [
+                          Image.asset(
+                            'assets/images/calendar.png',
+                            height: 24,
+                            width: 24,
+                          ),
+                          const SizedBox(width: 13.8),
+                          const Text('Today'),
+                        ]),
+                      ),
                     ),
                   ),
                   Padding(
@@ -196,5 +219,175 @@ class _AddEmployeeDetailPageState extends State<AddEmployeeDetailPage> {
         ),
       ),
     );
+  }
+
+  selectDate() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Colors.white,
+        insetPadding: const EdgeInsets.symmetric(horizontal: 16.0),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 0, vertical: 20.0),
+        content: SizedBox(
+          height: 544,
+          width: 396,
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                      child: _presetButton("Today", DateTime.now(), false)),
+                  const SizedBox(width: 16),
+                  Expanded(
+                      child: _presetButton("Next Monday", _nextMonday(), true)),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                      child:
+                          _presetButton("Next Tuesday", _nextTuesday(), false)),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: _presetButton("After 1 week",
+                        DateTime.now().add(const Duration(days: 7)), false),
+                  ),
+                ],
+              ),
+            ),
+            // Table Calendar
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: TableCalendar(
+                  focusedDay: _selectedDate,
+                  firstDay: DateTime(2000),
+                  lastDay: DateTime(2100),
+                  calendarFormat: _calendarFormat,
+                  selectedDayPredicate: (day) => isSameDay(day, _selectedDate),
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectedDate = selectedDay;
+                    });
+                  },
+                  headerStyle: const HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                  ),
+                  availableGestures: AvailableGestures.all,
+                ),
+              ),
+            ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    'assets/images/calendar.png',
+                    height: 20,
+                    width: 23,
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    formatter.format(_selectedDate),
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.w400),
+                  ),
+                  const Spacer(),
+                  GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Container(
+                      height: 40,
+                      width: 73,
+                      decoration: const BoxDecoration(
+                          color: Color(0xffEDF8FF),
+                          borderRadius: BorderRadius.all(Radius.circular(4))),
+                      child: const Center(child: Text("Cancel")),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                            content: Text(
+                                "Selected Date: ${formatter.format(_selectedDate)}")),
+                      );
+                    },
+                    child: Container(
+                      height: 40,
+                      width: 73,
+                      decoration: const BoxDecoration(
+                          color: Color(0xff1DA1F2),
+                          borderRadius: BorderRadius.all(Radius.circular(4))),
+                      child: const Center(child: Text("Save")),
+                    ),
+                  ),
+                  // ElevatedButton(
+                  //   child: const Text("Save"),
+                  //   onPressed: () {
+                  //     Navigator.pop(context);
+                  //     ScaffoldMessenger.of(context).showSnackBar(
+                  //       SnackBar(
+                  //           content: Text(
+                  //               "Selected Date: ${formatter.format(_selectedDate)}")),
+                  //     );
+                  //   },
+                  // ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  /// Button for selecting preset dates
+  Widget _presetButton(String text, DateTime date, bool isSelected) {
+    return GestureDetector(
+      onTap: () => setState(() {
+        _selectedDate = date;
+      }),
+      child: Container(
+        height: 36,
+        padding: EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          color: isSelected ? const Color(0xff1DA1F2) : const Color(0xffEDF8FF),
+          borderRadius: const BorderRadius.all(Radius.circular(4)),
+        ),
+        child: Center(
+          child: Text(text,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: isSelected ? Colors.white : const Color(0xff1DA1F2),
+              )),
+        ),
+      ),
+    );
+  }
+
+  /// Get next Monday's date
+  DateTime _nextMonday() {
+    DateTime now = DateTime.now();
+    return now.add(Duration(days: (8 - now.weekday) % 7));
+  }
+
+  /// Get next Tuesday's date
+  DateTime _nextTuesday() {
+    DateTime now = DateTime.now();
+    return now.add(Duration(days: (9 - now.weekday) % 7));
   }
 }
---
+++ lib/features/retrieve_employee/presentation/widgets/app_calendar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class AppCalendar extends StatelessWidget {
+  const AppCalendar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
